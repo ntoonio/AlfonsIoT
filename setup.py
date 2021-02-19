1812378e7782c2c4bed31413cd84386ac350c376
Anton Lindroth <ntoonio@gmail.com>
2021-02-19 20:57:05 +0100

    Cleaned setup.py. Bumped

--- setup.py
+++ setup.py
@@ -4,22 +4,12 @@ import re
 
 PATH = os.path.dirname(os.path.abspath(__file__)) + "/"
 
-installRequires = []
-
-with open(PATH + "requirements.txt", "r") as f:
-	for l in f.readlines():
-		l = re.sub("(#|-e).*$", "", l) # Remove comments and lines that start with -e
-		l = l.rstrip().lstrip()
-
-		if not l == "": # Empty lines (and commented lines which now are empty) will be removed
-			installRequires.append(l)
-
 with open(PATH + "README.md", "r") as fh:
 	long_description = fh.read()
 
 setuptools.setup(
 	name="alfonsiot",
-	version="0.0.7",
+	version="0.0.10",
 	author="Anton Lindroth",
 	author_email="ntoonio@gmail.com",
 	description="A package for IoTs to interact with Alfons",
@@ -27,5 +17,8 @@ setuptools.setup(
 	long_description_content_type="text/markdown",
 	url="https://github.com/ntoonio/AlfonsIoT",
 	packages=setuptools.find_packages(),
-	install_requires=installRequires
+	install_requires=[
+		"paho-mqtt==1.5.0",
+		"requests==2.25.1"
+	]
 )
